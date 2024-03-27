98d77ff647fdacbe76dac3533e7de1099973c9c5
metaboxdev <165104387+metaboxdev@users.noreply.github.com>
2024-03-27 19:42:20 +0100

    first commit
    first commit

---
+++ assets/index-CJRkCOx9.js
@@ -0,0 +1,9 @@
+// import{cb as t,bJ as e,cf as i,cg as n,ci as c,cc as s,ch as f,ce as d,cd as l}from"./index-D6MoQ72h.js";const a=`
+// @font-face {
+//   font-family: 'Inter';
+//   font-style:  normal;
+//   font-weight: 300 600;
+//   font-display: swap;
+//   src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
+// }
+// `;export{a as InterVar,t as ProviderRpcError,e as ProviderRpcErrorCode,i as chainIdValidation,n as chainNamespaceValidation,c as chainValidation,s as createEIP1193Provider,f as providerConnectionInfoValidation,d as validate,l as weiToEth};
